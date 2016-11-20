a433f6c60acc55a5b3c10e461c077f28c579fe58
Ivan Zubenko <vanya.zubenko@gmail.com>
2016-11-20 14:19:29 +0200

    sufix array optimization

--- Algorithms/PatternMatching/SuffixArrays/SuffixArray.cs
+++ Algorithms/PatternMatching/SuffixArrays/SuffixArray.cs
@@ -16,28 +16,69 @@ namespace Algorithms.PatternMatching.SuffixArrays
             }
 
             suffixArray = CreateSuffixArray(text);
-
-            string[] suffixes = suffixArray.Select(x => text.Substring(x)).ToArray();
         }
 
         private static int[] CreateSuffixArray(string text)
         {
-            var previousLabels = new LabelsPair[text.Length];
             var labels = new LabelsPair[text.Length];
+            var nextLabels = new LabelsPair[text.Length];
+            var sortBuffer = new LabelsPair[text.Length];
+            var ranks = new int[text.Length];
 
             for (int i = 0; i < text.Length; ++i)
             {
-                previousLabels[i] = new LabelsPair(i, text[i]);
+                labels[i] = new LabelsPair(i, text[i]);
             }
 
-            RadixSort.Sort(previousLabels, x => x.Label1);
+            for (int i = 0; i < labels.Length; ++i)
+            {
+                ranks[i] = i;
+            }
 
-            LabelsPair previous = previousLabels[0];
+            for (int length = 1; length < text.Length;)
+            {
+                length = Math.Min(length * 2, text.Length);
+
+                for (int i = 0; i < ranks.Length - length / 2; ++i)
+                {
+                    int index1 = ranks[i];
+                    int index2 = ranks[i + length / 2];
+                    nextLabels[i] = new LabelsPair(i, labels[index1].Label1, labels[index2].Label1);
+                }
+
+                for (int i = ranks.Length - length / 2; i < ranks.Length; ++i)
+                {
+                    int index1 = ranks[i];
+                    nextLabels[i] = new LabelsPair(i, labels[index1].Label1);
+                }
+
+                Swap(ref labels, ref nextLabels);
+
+                RadixSort.Sort(labels, sortBuffer, x => x.Label2);
+                RadixSort.Sort(labels, sortBuffer, x => x.Label1);
+
+                MarkWithLabels(labels);
+                UpdateRanks(ranks, labels);
+            }
+
+            int[] result = ranks;
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = labels[i].SourceIndex;
+            }
+
+            return result;
+        }
+
+        private static void MarkWithLabels(LabelsPair[] labels)
+        {
+            LabelsPair previous = labels[0];
             int currentLabel = 0;
 
-            for (int i = 0; i < previousLabels.Length; ++i)
+            for (int i = 0; i < labels.Length; ++i)
             {
-                LabelsPair label = previousLabels[i];
+                LabelsPair label = labels[i];
 
                 if (previous.Label1 != label.Label1 || previous.Label2 != label.Label2)
                 {
@@ -45,72 +86,16 @@ namespace Algorithms.PatternMatching.SuffixArrays
                     previous = label;
                 }
 
-                previousLabels[i] = new LabelsPair(label.SourceIndex, currentLabel);
+                labels[i] = new LabelsPair(label.SourceIndex, currentLabel);
             }
+        }
 
-            var ranks = new int[previousLabels.Length];
-            for (int i = 0; i < previousLabels.Length; ++i)
+        private static void UpdateRanks(int[] ranks, LabelsPair[] labels)
+        {
+            for (int i = 0; i < labels.Length; ++i)
             {
-                ranks[previousLabels[i].SourceIndex] = i;
+                ranks[labels[i].SourceIndex] = i;
             }
-
-            for (int length = 2; ; length *= 2)
-            {
-                length = Math.Min(length, text.Length);
-
-                for (int i = 0; i < ranks.Length - length / 2; ++i)
-                {
-                    int index1 = ranks[i];
-                    int index2 = ranks[i + length / 2];
-                    labels[i] = new LabelsPair(i, previousLabels[index1].Label1, previousLabels[index2].Label1);
-                }
-
-                for (int i = ranks.Length - length / 2; i < ranks.Length; ++i)
-                {
-                    int index1 = ranks[i];
-                    labels[i] = new LabelsPair(i, previousLabels[index1].Label1);
-                }
-
-                RadixSort.Sort(labels, x => x.Label2);
-                RadixSort.Sort(labels, x => x.Label1);
-
-                previous = labels[0];
-                currentLabel = 0;
-
-                for (int i = 0; i < labels.Length; ++i)
-                {
-                    LabelsPair label = labels[i];
-
-                    if (previous.Label1 != label.Label1 || previous.Label2 != label.Label2)
-                    {
-                        ++currentLabel;
-                        previous = label;
-                    }
-
-                    labels[i] = new LabelsPair(label.SourceIndex, currentLabel);
-                }
-
-                for (int i = 0; i < labels.Length; ++i)
-                {
-                    ranks[labels[i].SourceIndex] = i;
-                }
-
-                Swap(ref previousLabels, ref labels);
-
-                if (length == text.Length)
-                {
-                    break;
-                }
-            }
-
-            int[] result = ranks;
-
-            for (int i = 0; i < result.Length; i++)
-            {
-                result[i] = previousLabels[i].SourceIndex;
-            }
-
-            return result;
         }
 
         private static void Swap<T>(ref T arg1, ref T arg2)
--- Algorithms/Sorting/RadixSort.cs
+++ Algorithms/Sorting/RadixSort.cs
@@ -4,39 +4,16 @@ namespace Algorithms.Sorting
 {
     public static class RadixSort
     {
-        public static void Sort<TElement>(TElement[] array, Func<TElement, int> toInt)
+        public static void Sort<TElement>(TElement[] array, TElement[] buffer, Func<TElement, int> toInt)
         {
             TElement[] source = array;
-            var dest = new TElement[source.Length];
             var digitCounts = new int[10];
             bool hasDigits = true;
 
             for (int digitNumber = 0; hasDigits; ++digitNumber)
             {
-                hasDigits = false;
-                int m = (int)Math.Pow(10, digitNumber + 1);
-                int n = m / 10;
-
-                for (int i = 0; i < source.Length; ++i)
-                {
-                    int digit = toInt(source[i]) % m / n;
-                    ++digitCounts[digit];
-                    hasDigits |= digit > 0;
-                }
-
-                for (int i = 1; i < 10; i++)
-                {
-                    digitCounts[i] += digitCounts[i - 1];
-                }
-
-                for (int i = source.Length - 1; i >= 0; --i)
-                {
-                    int digit = toInt(source[i]) % m / n;
-                    --digitCounts[digit];
-                    dest[digitCounts[digit]] = source[i];
-                }
-
-                Swap(ref source, ref dest);
+                hasDigits = CountingSort(source, buffer, toInt, digitCounts, digitNumber);
+                Swap(ref source, ref buffer);
                 Array.Clear(digitCounts, 0, 10);
             }
 
@@ -44,6 +21,40 @@ namespace Algorithms.Sorting
             {
                 Array.Copy(source, array, source.Length);
             }
+        }
+
+        public static void Sort<TElement>(TElement[] array, Func<TElement, int> toInt)
+        {
+            Sort(array, new TElement[array.Length], toInt);
+        }
+
+        private static bool CountingSort<TElement>(TElement[] source, TElement[] dest, Func<TElement, int> toInt,
+            int[] digitCounts, int digitNumber)
+        {
+            int m = (int)Math.Pow(10, digitNumber + 1);
+            int n = m / 10;
+            bool hasDigits = false;
+
+            for (int i = 0; i < source.Length; ++i)
+            {
+                int digit = toInt(source[i]) % m / n;
+                ++digitCounts[digit];
+                hasDigits |= digit > 0;
+            }
+
+            for (int i = 1; i < 10; i++)
+            {
+                digitCounts[i] += digitCounts[i - 1];
+            }
+
+            for (int i = source.Length - 1; i >= 0; --i)
+            {
+                int digit = toInt(source[i]) % m / n;
+                --digitCounts[digit];
+                dest[digitCounts[digit]] = source[i];
+            }
+
+            return hasDigits;
         }
 
         private static void Swap<T>(ref T arg1, ref T arg2)

7d5d20fbb820f7cba1cb86bbade880423b30b727
Ivan Zubenko <vanya.zubenko@gmail.com>
2016-11-24 00:25:11 +0200

    karkkainen sanders refactoring

--- Algorithms/PatternMatching/SuffixArrays/KarkkainenSanders/KarkkainenSandersConstructor.cs
+++ Algorithms/PatternMatching/SuffixArrays/KarkkainenSanders/KarkkainenSandersConstructor.cs
@@ -7,7 +7,7 @@ namespace Algorithms.PatternMatching.SuffixArrays.KarkkainenSanders
         public int[] Create(string text)
         {
             var labels = new Label[text.Length];
-            var ranks = new int[text.Length];
+            var ranks = new int[text.Length + 1];
 
             for (int i = 0; i < labels.Length; ++i)
             {
@@ -17,156 +17,40 @@ namespace Algorithms.PatternMatching.SuffixArrays.KarkkainenSanders
             return CreateSuffixArray(labels, ranks);
         }
 
-        private static int[] CreateSuffixArray(Label[] previousLabels, int[] ranks)
+        private static int[] CreateSuffixArray(Label[] previousLabels, int[] ranks01)
         {
             if (previousLabels.Length < 3)
             {
                 return CreateSimpleSuffixArray(previousLabels);
             }
 
-            Triple[] triples = CreateTriples(previousLabels);
-            Label[] labels = CreateLabels(triples, previousLabels);
+            Label[] labels = CreateLabels(previousLabels);
             Label[] labels01 = CreateLabels01(labels);
 
-            int[] suffixArray01 = CreateSuffixArray(labels01, ranks);
+            int[] suffixArray01 = CreateSuffixArray(labels01, ranks01);
 
             for (int i = 0; i < suffixArray01.Length; i++)
             {
                 suffixArray01[i] = labels01[suffixArray01[i]].SourceIndex;
             }
 
-            FillRanks(ranks, suffixArray01);
+            FillRanks(ranks01, suffixArray01);
 
-            var labels2 = new LabelsPair[previousLabels.Length / 3];
-            for (int i = 2; i < previousLabels.Length - 1; i += 3)
-            {
-                labels2[i / 3] = new LabelsPair(i, previousLabels[i].Value, ranks[i + 1]);
-            }
+            int[] suffixArray2 = CreateSuffixArray2(previousLabels, ranks01);
 
-            if (labels.Length % 3 == 0)
-            {
-                int i = previousLabels.Length - 1;
-                labels2[labels2.Length - 1] = new LabelsPair(i, previousLabels[i].Value, 0);
-            }
-
-            RadixSort.Sort(labels2, x => x.Label2);
-            RadixSort.Sort(labels2, x => x.Label1);
-
-            var suffixArray = new int[labels.Length];
-            int index01 = 0;
-            int index2 = 0;
-
-            for (int i = 0; i < suffixArray.Length; i++)
-            {
-                if (index2 == labels2.Length)
-                {
-                    suffixArray[i] = suffixArray01[index01];
-                    ++index01;
-                    continue;
-                }
-                if (index01 == suffixArray01.Length)
-                {
-                    suffixArray[i] = labels2[index2].SourceIndex;
-                    ++index2;
-                    continue;
-                }
-
-                int suffix01 = suffixArray01[index01];
-                int suffix2 = labels2[index2].SourceIndex;
-
-                if (previousLabels[suffix01].Value < previousLabels[suffix2].Value)
-                {
-                    ++index01;
-                    suffixArray[i] = suffix01;
-                }
-                else if (previousLabels[suffix01].Value > previousLabels[suffix2].Value)
-                {
-                    ++index2;
-                    suffixArray[i] = suffix2;
-                }
-                else if (suffix01 == previousLabels.Length - 1)
-                {
-                    ++index01;
-                    suffixArray[i] = suffix01;
-                }
-                else if (suffix2 == previousLabels.Length - 1)
-                {
-                    ++index2;
-                    suffixArray[i] = suffix2;
-                }
-                else
-                {
-                    if (suffix01 % 3 == 0)
-                    {
-                        if (ranks[suffix01 + 1] < ranks[suffix2 + 1])
-                        {
-                            ++index01;
-                            suffixArray[i] = suffix01;
-                        }
-                        else
-                        {
-                            ++index2;
-                            suffixArray[i] = suffix2;
-                        }
-                    }
-                    else
-                    {
-                        suffix01 += 1;
-                        suffix2 += 1;
-
-                        if (previousLabels[suffix01].Value < previousLabels[suffix2].Value
-                            || suffix01 == previousLabels.Length - 1)
-                        {
-                            ++index01;
-                            suffixArray[i] = suffix01 - 1;
-                        }
-                        else if (previousLabels[suffix01].Value > previousLabels[suffix2].Value
-                                 || suffix2 == previousLabels.Length - 1)
-                        {
-                            ++index2;
-                            suffixArray[i] = suffix2 - 1;
-                        }
-                        else
-                        {
-                            if (ranks[suffix01 + 1] < ranks[suffix2 + 1])
-                            {
-                                ++index01;
-                                suffixArray[i] = suffix01 - 1;
-                            }
-                            else
-                            {
-                                ++index2;
-                                suffixArray[i] = suffix2 - 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            return suffixArray;
+            return Merge(suffixArray01, suffixArray2, ranks01, previousLabels);
         }
 
-        private static Triple[] CreateTriples(Label[] labels)
+        private static Label[] CreateLabels(Label[] labels)
         {
-            var triples = new Triple[labels.Length];
-
-            for (int i = 0; i < labels.Length; ++i)
-            {
-                triples[i] = new Triple(i, i + 2);
-            }
-
-            return triples;
-        }
-
-        private static Label[] CreateLabels(Triple[] triples, Label[] labels)
-        {
+            Triple[] triples = CreateTriples(labels);
             var sortBuffer = new Triple[triples.Length];
+
             RadixSort.Sort(triples, sortBuffer, x => x.Label3(labels));
             RadixSort.Sort(triples, sortBuffer, x => x.Label2(labels));
             RadixSort.Sort(triples, sortBuffer, x => x.Label1(labels));
 
             var result = new Label[triples.Length];
-
             int currentLabael = 0;
             Triple previous = triples[0];
 
@@ -186,6 +70,18 @@ namespace Algorithms.PatternMatching.SuffixArrays.KarkkainenSanders
             RadixSort.Sort(result, x => x.SourceIndex);
 
             return result;
+        }
+
+        private static Triple[] CreateTriples(Label[] labels)
+        {
+            var triples = new Triple[labels.Length];
+
+            for (int i = 0; i < labels.Length; ++i)
+            {
+                triples[i] = new Triple(i, i + 2);
+            }
+
+            return triples;
         }
 
         private static Label[] CreateLabels01(Label[] labels)
@@ -217,6 +113,21 @@ namespace Algorithms.PatternMatching.SuffixArrays.KarkkainenSanders
             }
         }
 
+        private static int[] CreateSuffixArray2(Label[] labels, int[] ranks01)
+        {
+            var suffixArray2 = new int[labels.Length / 3];
+
+            for (int i = 2; i < labels.Length; i += 3)
+            {
+                suffixArray2[i / 3] = i;
+            }
+
+            RadixSort.Sort(suffixArray2, i => ranks01[i + 1]);
+            RadixSort.Sort(suffixArray2, i => labels[i].Value);
+
+            return suffixArray2;
+        }
+
         private static int[] CreateSimpleSuffixArray(Label[] labels)
         {
             if (labels.Length == 1)
@@ -227,6 +138,102 @@ namespace Algorithms.PatternMatching.SuffixArrays.KarkkainenSanders
             return labels[0].Value < labels[1].Value
                 ? new[] { 0, 1 }
                 : new[] { 1, 0 };
+        }
+
+        private static int[] Merge(int[] suffixArray01, int[] suffixArray2, int[] ranks01, Label[] labels)
+        {
+            var suffixArray = new int[labels.Length];
+            int index01 = 0;
+            int index2 = 0;
+
+            for (int i = 0; i < suffixArray.Length; i++)
+            {
+                if (index2 == suffixArray2.Length)
+                {
+                    suffixArray[i] = suffixArray01[index01];
+                    ++index01;
+                    continue;
+                }
+                if (index01 == suffixArray01.Length)
+                {
+                    suffixArray[i] = suffixArray2[index2];
+                    ++index2;
+                    continue;
+                }
+
+                int suffix01 = suffixArray01[index01];
+                int suffix2 = suffixArray2[index2];
+
+                if (labels[suffix01].Value < labels[suffix2].Value)
+                {
+                    ++index01;
+                    suffixArray[i] = suffix01;
+                }
+                else if (labels[suffix01].Value > labels[suffix2].Value)
+                {
+                    ++index2;
+                    suffixArray[i] = suffix2;
+                }
+                else if (suffix01 == labels.Length - 1)
+                {
+                    ++index01;
+                    suffixArray[i] = suffix01;
+                }
+                else if (suffix2 == labels.Length - 1)
+                {
+                    ++index2;
+                    suffixArray[i] = suffix2;
+                }
+                else
+                {
+                    if (suffix01 % 3 == 0)
+                    {
+                        if (ranks01[suffix01 + 1] < ranks01[suffix2 + 1])
+                        {
+                            ++index01;
+                            suffixArray[i] = suffix01;
+                        }
+                        else
+                        {
+                            ++index2;
+                            suffixArray[i] = suffix2;
+                        }
+                    }
+                    else
+                    {
+                        suffix01 += 1;
+                        suffix2 += 1;
+
+                        if (labels[suffix01].Value < labels[suffix2].Value
+                            || suffix01 == labels.Length - 1)
+                        {
+                            ++index01;
+                            suffixArray[i] = suffix01 - 1;
+                        }
+                        else if (labels[suffix01].Value > labels[suffix2].Value
+                                 || suffix2 == labels.Length - 1)
+                        {
+                            ++index2;
+                            suffixArray[i] = suffix2 - 1;
+                        }
+                        else
+                        {
+                            if (ranks01[suffix01 + 1] < ranks01[suffix2 + 1])
+                            {
+                                ++index01;
+                                suffixArray[i] = suffix01 - 1;
+                            }
+                            else
+                            {
+                                ++index2;
+                                suffixArray[i] = suffix2 - 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return suffixArray;
         }
     }
 }
